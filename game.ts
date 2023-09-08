dc96454d86fc9a6f64a0889e8ddfcddf66d37a1e
Tooba Riaz <riazelecteng@gmail.com>
2023-09-08 17:46:58 +0500

    files added

---
+++ game.ts
@@ -0,0 +1,25 @@
+import inquirer from 'inquirer';
+let systemgeneratedNo = Math.floor(Math.random()*17);
+console.log(systemgeneratedNo)
+
+type anytype = {
+    userguess :number
+}
+let answers : anytype= await inquirer.prompt(
+    [
+        {
+            type : "number",
+            name : "userguess",
+            message : "write your guess b/w 1--10"
+        }
+    ]
+)
+let {userguess} = answers
+if(userguess == systemgeneratedNo)
+{
+    console.log(userguess , "userguess" , systemgeneratedNo, "Sys")
+    console.log("yeah your answer is correct  \n you win!")
+}
+else{
+    console.log("plz try again better luck next time")
+}
